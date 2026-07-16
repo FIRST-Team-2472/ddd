5207b4f99bf6f4bad44877084f456e170680a392
GBlast2000 <kwdierks@gmail.com>
2026-07-16 18:42:00 -0500

    Aimbot now waits for visible target to run/end
    just in case I guess

--- src/main/java/frc/robot/commands/AimBot.java
+++ src/main/java/frc/robot/commands/AimBot.java
@@ -7,25 +7,25 @@ import frc.robot.generated.LimelightHelpers;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
-// Not sure if importing these is what I should be doing but they're stopping the errors so...
-import static edu.wpi.first.units.Units.RadiansPerSecond;
-import static edu.wpi.first.units.Units.RotationsPerSecond;;
+//import static edu.wpi.first.units.Units.RadiansPerSecond;
+//import static edu.wpi.first.units.Units.RotationsPerSecond;;
 
 public class AimBot extends Command {
 
       private CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
-      private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond);
+      //private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond);
 
       private double targetOffset;
+      private boolean targetVisible;
 
-      // KP is constant but im too lazy to make a constants file just for this
+      // kP is constant but I'm too lazy to make a constants file just for kP
       private double kP = 0.03;
 
       private final SwerveRequest.FieldCentric m_driveRequest = new SwerveRequest.FieldCentric()
             .withSteerRequestType(SteerRequestType.Position);
 
       public AimBot() {
-
+            addRequirements(drivetrain);
       }
 
       @Override
@@ -35,19 +35,22 @@ public class AimBot extends Command {
 
       @Override
       public void execute() {
-            targetOffset = LimelightHelpers.getTX("limelight");
-            if (targetOffset > 1) {
-                  drivetrain.applyRequest(() ->
-                        m_driveRequest.withRotationalRate(Math.max(-kP * targetOffset, -0.5)));
-            } else if (targetOffset < -1) {
-                  drivetrain.applyRequest(() ->
-                        m_driveRequest.withRotationalRate(Math.min(kP * targetOffset, 0.5)));
+            targetVisible = LimelightHelpers.getTV("limelight");
+            if (targetVisible) {
+                  targetOffset = LimelightHelpers.getTX("limelight");
+                  if (targetOffset > 1) {
+                        drivetrain.applyRequest(() ->
+                              m_driveRequest.withRotationalRate(Math.max(-kP * targetOffset, -0.5)));
+                  } else if (targetOffset < -1) {
+                        drivetrain.applyRequest(() ->
+                              m_driveRequest.withRotationalRate(Math.min(kP * targetOffset, 0.5)));
+                  }
             }
       }
 
       @Override
       public boolean isFinished() {
-            return -1 <= targetOffset && targetOffset <= 1;
+            return targetVisible && -1 <= targetOffset && targetOffset <= 1;
       }
 
       @Override
