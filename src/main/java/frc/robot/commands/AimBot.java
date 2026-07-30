2325fd0a4b8ba9885218600ff8b6fd439bf64c1d
Hoplite30000 <97264604+Hoplite30000@users.noreply.github.com>
2026-07-30 16:57:06 -0500

    ready to test
    67

--- src/main/java/frc/robot/commands/AimBot.java
+++ src/main/java/frc/robot/commands/AimBot.java
@@ -5,32 +5,28 @@ import static edu.wpi.first.units.Units.RotationsPerSecond;
 import com.ctre.phoenix6.swerve.SwerveModule.SteerRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.LimelightHelpers;
-import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
-
-import com.ctre.phoenix6.hardware.Pigeon2;
 
 import static edu.wpi.first.units.Units.RadiansPerSecond;
 
 public class Aimbot extends Command {
-    Timer timer = new Timer();
     private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond);
-    private final Pigeon2 pigeon2 = new Pigeon2(0);
 
     private final SwerveRequest.FieldCentric m_driveRequest = new SwerveRequest.FieldCentric()
             .withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
             .withSteerRequestType(SteerRequestType.Position);
 
-    public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+
+    public final CommandSwerveDrivetrain drivetrain;
 
     double xoffset;
     double sumError;
     double previousError;
 
-    public Aimbot() {
+    public Aimbot(CommandSwerveDrivetrain drivetrain) {
+        this.drivetrain = drivetrain;
         addRequirements(drivetrain);
     }
 
@@ -38,19 +34,21 @@ public class Aimbot extends Command {
     public void initialize() {
         sumError = 0;
         previousError = 0;
-        timer.restart();
     }
 
     @Override
     public void execute() {
-        xoffset = LimelightHelpers.getTX("limelight");
-        sumError += xoffset;
-        double kp = 0;
+        xoffset = LimelightHelpers.getTX("limelight-three");
+        if (!LimelightHelpers.getTV("limelight-three")) {
+            return;
+        }
+        sumError += xoffset * 0.02;
+        double kp = 0.1;
         double ki = 0;
         double kd = 0;
-        double power = (kp*xoffset) + (ki * sumError) + (xoffset - previousError)/timer.get() * kd;
-    
-        drivetrain.applyRequest(() -> m_driveRequest.withRotationalRate(0.2*power));
+        double power = Math.min(1, Math.max(-1, (kp * xoffset) + (ki * sumError) + (xoffset - previousError)/0.02 * kd));
+
+        drivetrain.setControl(m_driveRequest.withRotationalRate(power * MaxAngularRate));
         previousError = xoffset;
     }
 
@@ -61,6 +59,6 @@ public class Aimbot extends Command {
 
     @Override
     public void end(boolean interrupted) {
-        drivetrain.applyRequest(() -> m_driveRequest.withRotationalRate(0.0));
+        drivetrain.setControl(m_driveRequest.withRotationalRate(0.0));
     }
 }
